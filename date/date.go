4fc86cdbf2f4b0c2ccff4384a1fa1e9a9203c87c
Alex Palaistras <alex@deuill.org>
2018-05-12 14:20:31 +0100

    date: Allow for second-precision date formats
    Previously, a date format given with seconds would only still update every minute.

--- date/date.go
+++ date/date.go
@@ -13,24 +13,41 @@ import (
 type Date struct {
 	Format *string // The date format to use.
 	Icon   *string // Code-point for date icon.
-	msg    statusbar.Message
+
+	wait time.Duration
+	msg  statusbar.Message
 }
 
 // Run returns a message containing the current date, according to the
 // pre-configured format.
 func (d *Date) Run() *statusbar.Message {
 	d.msg.Text = *d.Icon + " " + time.Now().Format(*d.Format)
-
 	return &d.msg
 }
 
 // Wait sleeps until the date minute changes.
 func (d *Date) Wait() {
-	time.Sleep(time.Duration(60-time.Now().Second()) * time.Second)
+	switch d.wait {
+	case time.Second:
+		time.Sleep(1 * time.Second)
+	case time.Minute:
+		time.Sleep(time.Duration((60 - time.Now().Second())) * time.Second)
+	}
 }
 
 // Init processes post-registration operations.
 func (d *Date) Init() error {
+	t, err := time.Parse(*d.Format, *d.Format)
+	if err != nil {
+		return err
+	}
+
+	// Override default wait interval if format precision is less than the default
+	// of one minute.
+	if t.Second() > 0 {
+		d.wait = time.Second
+	}
+
 	return nil
 }
 
@@ -40,6 +57,7 @@ func New() *statusbar.Applet {
 	date := &Date{
 		Format: flags.String("format", "Mon 2 Jan, 15:04", ""),
 		Icon:   flags.String("icon", "", ""),
+		wait:   time.Minute,
 	}
 
 	applet := statusbar.NewApplet("date", date)
